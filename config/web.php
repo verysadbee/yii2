bef72b38e21b4e5e89553d170afc0fbc2e9594d7
verysadbee <116225794+verysadbee@users.noreply.github.com>
2024-02-21 20:12:38 +0300

    Последние изменения на авторизацию

--- config/web.php
+++ config/web.php
@@ -14,8 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'bee',
-            'baseUrl' => '',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,12 +42,14 @@ $config = [
             ],
         ],
         'db' => $db,
+        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
+        */
     ],
     'params' => $params,
 ];
